efe6515cf22bb843137191bf183c4411071c23fb
Rohan Karmakar <rohankarmakar2356@gmail.com>
2022-01-29 14:34:46 +0530

    searchbox created

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
+import {Card} from '../card/card.component'
 import './card-list.css';
 
 export const CardList = (props) => {
-    return(<h1 className='card-list'>Hello {props.name}</h1>)
+    return(
+        <div className='card-list'>
+            { props.monsters.map(monster =>
+                (
+                    <Card key={monster.id} monster={monster} />
+                )
+            )
+        }
+        </div>
+    )
 };
